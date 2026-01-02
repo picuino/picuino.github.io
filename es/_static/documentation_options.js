c34f21b20a1ad41e8d5312b579dffc2868181336
Carlos Pardo <carlos@picuino.com>
2026-01-02 10:38:21 +0100

    Change to 2026

--- es/_static/documentation_options.js
+++ es/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2025',
+    VERSION: '2026',
     LANGUAGE: 'es',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

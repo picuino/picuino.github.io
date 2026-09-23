520c808c39a5dd89eee4f05901b1b50e6ce38e19
Carlos <carlos@picuino.com>
2026-09-23 19:10:22 +0200

    2026-09-23 Update

---
+++ en/_static/_thumbnails.bat
@@ -0,0 +1,45 @@
+set OPTIONS=-level 0%%,95%% -fuzz 2%% -transparent White -resize 480x
+
+set IMAGE=index-antena
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-circuito
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-crane
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-dibujo
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-electronica
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-herramientas
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-ladrillos
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-ordenador
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-programa
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-robot
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-arduino
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-balanza
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-test
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+set IMAGE=index-simulador
+\Bin\ImageMagick\convert %IMAGE%-org.png %OPTIONS% %IMAGE%.webp
+
+pause
